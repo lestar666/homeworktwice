ac160c4a6fb16f595f99a7b3c66ca7e6a57425c9
2077946749 <2077946749@qq.com>
2020-02-28 14:48:51 +0800

    binarization

---
+++ Binarization/Binarization.cpp
@@ -0,0 +1,33 @@
+﻿// Binarization.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <opencv.hpp>
+
+using namespace cv;
+
+int main()
+{
+	cv::Mat srcMat = imread("D:\\testImage\\1.jpg");
+	int height = srcMat.rows;//行数为高
+	int width = srcMat.cols;//列数为宽
+
+	for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width; i++) {
+			uchar threshold = 100;
+			uchar average = (srcMat.at<Vec3b>(j, i)[0] +
+				srcMat.at<Vec3b>(j, i)[1] +
+				srcMat.at<Vec3b>(j, i)[2]) / 3;
+			if (average > threshold)
+				average = 255;
+			else
+				average = 0;
+			srcMat.at<Vec3b>(j, i)[0] = average;
+			srcMat.at<Vec3b>(j, i)[1] = average;
+			srcMat.at<Vec3b>(j, i)[2] = average;
+		}
+	}
+	imshow("src", srcMat);
+	waitKey(0);
+	return 1;
+}
